2d652e6c957ce37b9767894625460ce1571f279b
JackShort <john.short.tj@gmail.com>
2023-10-26 10:46:27 -0400

    maybe grid

---
+++ GridRenderer.gd
@@ -0,0 +1,17 @@
+extends ColorRect
+
+# @export var grid_step := 100
+
+# func _draw_vertical_line(x_offset: int):
+#     draw_line(Vector2(x_offset, 0), Vector2(x_offset, size.y), Color.RED)
+
+# func _draw_horizontal_line(y_offset: int):
+#     draw_line(Vector2(0, y_offset), Vector2(size.x, y_offset), Color.RED)
+
+# func _draw():
+#     for i in int(size.x):
+#         if i % grid_step == 0:
+#             _draw_vertical_line(i)
+#     for i in int(size.y):
+#         if i % grid_step == 0:
+#             _draw_horizontal_line(i)
