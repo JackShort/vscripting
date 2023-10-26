7c031a799c3998534292a527135ab3e54c8ce4c1
JackShort <john.short.tj@gmail.com>
2023-10-26 17:34:25 -0400

    main to graph rename

--- Global.gd
+++ Global.gd
@@ -1,13 +1,11 @@
 extends Node
 
 enum SolTypes {
-    sol_uint8,
     sol_uint256,
     sol_bool
 }
 
 var sol_types_to_string = {
-    SolTypes.sol_uint8: "uint8",
     SolTypes.sol_uint256: "uint256",
     SolTypes.sol_bool: "bool",
 }
