6b6509f42154830ddc12ea331d5f6339fef9d979
JackShort <john.short.tj@gmail.com>
2023-10-26 11:46:06 -0400

    graphnode

---
+++ GraphNode.gd
@@ -0,0 +1,6 @@
+extends PanelContainer
+
+@onready var node_name_label: Label = %NodeName
+
+func init():
+    node_name_label.text = "Placeholder"
--- Main.gd
+++ Main.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
+@onready var graph_node_scene = preload("res://GraphNode.tscn")
+
 func _input(event):
     if event is InputEventMouseButton:
         if event.button_index == MOUSE_BUTTON_RIGHT:
             _add_node(get_global_mouse_position())
 
 func _add_node(mouse_position: Vector2):
-    print(mouse_position)
+    var graph_node = graph_node_scene.instantiate()
+    add_child(graph_node)
+    graph_node.global_position = mouse_position
+    graph_node.init()
